3675ff5b78ecf8788b584f7c8b5970553dbcc002
Paul Simpson <prsimp@gmail.com>
2012-05-17 05:23:47 -0500

    Chasing down template error.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_not_found
-  rescue_from ActionView::TemplateError, with: :rescue_template_error
+  #rescue_from ActionView::TemplateError, with: :rescue_template_error
 
   protected
 
